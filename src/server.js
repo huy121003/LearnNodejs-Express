2aaa2b272da9a1cd9afdcc5daffec8ee412dfc76
Quang Huy <quanghuy2003.hh@gmail.com>
2024-12-26 09:52:40 +0700

    use docker/commect mysql

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const configViewEngine = require("./configs/viewEngine");
 const webRoutes = require("./routes/web");
-
+const mysql = require("mysql2");
 const app = express();
 const port = process.env.POST;
 const hostname = process.env.HOST_NAME;
@@ -10,6 +10,22 @@ const hostname = process.env.HOST_NAME;
 configViewEngine(app);
 //khai bao cac routes
 app.use("/", webRoutes);
+//test connection
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+connection.query("SELECT * FROM Users", function (err, rows, fields) {
+  if (err) {
+    console.error("Database query error:", err); // Hiển thị lỗi
+    return;
+  }
+  console.log("The solution is: ", rows); // Hiển thị dữ liệu
+  console.log("The fields are: ", fields);// Hiển thị thông tin field
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port http://${hostname}:${port}`);
